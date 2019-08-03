f5036479eb9218dddb74a036d15c9c812a50beed
Hossam <hossam.nasr99@gmail.com>
2019-08-04 00:16:38 +0200

    added firebase config object

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCQhBv01Inz9eX7bC52TJnO3KyZ0odg6b4",
+  authDomain: "hummustrain.firebaseapp.com",
+  databaseURL: "https://hummustrain.firebaseio.com",
+  projectId: "hummustrain",
+  storageBucket: "hummustrain.appspot.com",
+  messagingSenderId: "965342327738",
+  appId: "1:965342327738:web:4cfe7db949e93693"
+};
