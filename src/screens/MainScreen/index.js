100bf8c3fddb449f8edb643653ba6973fef0baa7
Hossam <hossam.nasr99@gmail.com>
2019-08-04 00:46:08 +0200

    removed commented out code

--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -5,32 +5,9 @@ import AddCartButton from "./components/AddCartButton";
 import SizePicker from "./components/SizePicker";
 import { addCart, onCartsUpdate } from "../../helpers";
 
-// addCart({ color: "blue", name: "Hossam" });
-
-// getCarts(response => {
-//   console.log("response:", response);
-// });
-
-// const initialCarts = [
-//   {
-//     id: 1,
-//     name: "Hossam",
-//     color: "#BBBBBB"
-//   },
-//   {
-//     id: 2,
-//     name: "Vas Geht's ab Broder",
-//     color: "#FFBB00"
-//   }
-// ];
-
 const MainScreen = () => {
   const [carts, setCarts] = useState([]);
   const [cartSize, setCartSize] = useState("M");
-
-  // const addCart = cart => {
-  //   setCarts([...carts, cart]);
-  // };
 
   const onButtonPress = () => {
     addCart({ name: "Testing", color: "#0077FF" });
