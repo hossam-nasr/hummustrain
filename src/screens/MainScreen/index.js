c8418383f671319547c8cb9309077a15724c6378
Hossam <hossam.nasr99@gmail.com>
2019-08-04 00:17:33 +0200

    MainScreen reads carts data from database!

--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -1,33 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Title, ButtonContainer } from "./styles";
 import Train from "./components/Train";
 import AddCartButton from "./components/AddCartButton";
 import SizePicker from "./components/SizePicker";
+import { addCart, onCartsUpdate } from "../../helpers";
 
-const initialCarts = [
-  {
-    id: 1,
-    name: "Hossam",
-    color: "#BBBBBB"
-  },
-  {
-    id: 2,
-    name: "Vas Geht's ab Broder",
-    color: "#FFBB00"
-  }
-];
+// addCart({ color: "blue", name: "Hossam" });
+
+// getCarts(response => {
+//   console.log("response:", response);
+// });
+
+// const initialCarts = [
+//   {
+//     id: 1,
+//     name: "Hossam",
+//     color: "#BBBBBB"
+//   },
+//   {
+//     id: 2,
+//     name: "Vas Geht's ab Broder",
+//     color: "#FFBB00"
+//   }
+// ];
 
 const MainScreen = () => {
-  const [carts, setCarts] = useState(initialCarts);
+  const [carts, setCarts] = useState([]);
   const [cartSize, setCartSize] = useState("M");
 
-  const addCart = cart => {
-    setCarts([...carts, cart]);
-  };
+  // const addCart = cart => {
+  //   setCarts([...carts, cart]);
+  // };
 
   const onButtonPress = () => {
-    addCart({ id: 5, name: "Testing", color: "#0077FF" });
+    addCart({ name: "Testing", color: "#0077FF" });
   };
+
+  useEffect(() => {
+    onCartsUpdate(carts => {
+      setCarts(carts);
+    });
+  }, []);
 
   return (
     <Container>
