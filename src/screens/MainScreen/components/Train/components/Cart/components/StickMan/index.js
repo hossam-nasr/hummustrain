7a34b73d9e5a5ec70b39649d978ff7b328555923
Hossam <hossam.nasr99@gmail.com>
2019-08-04 21:20:56 +0200

    separated body from image

--- src/screens/MainScreen/components/Train/components/Cart/components/StickMan/index.js
+++ src/screens/MainScreen/components/Train/components/Cart/components/StickMan/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { StickManImg } from "../../../../../../../../constants";
-import { Container, StickManBodyImg } from "./styles";
+import { StickManBody, StickManHead } from "../../../../../../../../constants";
+import {
+  Container,
+  StickManBodyImg,
+  StickManContainer,
+  StickManHeadImg
+} from "./styles";
 
 const StickMan = ({ cartSize }) => (
   <Container cartSize={cartSize}>
-    <StickManBodyImg src={StickManImg} cartSize={cartSize} />
+    <StickManContainer>
+      <StickManHeadImg src={StickManHead} cartSize={cartSize} />
+      <StickManBodyImg src={StickManBody} cartSize={cartSize} />
+    </StickManContainer>
   </Container>
 );
 
--- src/screens/MainScreen/components/Train/components/Cart/components/StickMan/styles.js
+++ src/screens/MainScreen/components/Train/components/Cart/components/StickMan/styles.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { themeGet } from "@styled-system/theme-get";
 
 export const Container = styled.div`
   display: flex;
@@ -8,7 +7,21 @@ export const Container = styled.div`
   justify-content: center;
 `;
 
+export const StickManContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+`;
+
 export const StickManBodyImg = styled.img`
   height: ${({ cartSize, theme }) =>
     theme.carts.dimensions[cartSize].height + "px"};
+`;
+
+export const StickManHeadImg = styled.img`
+  height: ${({ cartSize, theme }) =>
+    theme.carts.dimensions[cartSize].height * 0.3 + "px"};
+  border-radius: ${({ cartSize, theme }) =>
+    (theme.carts.dimensions[cartSize].height * 0.3) / 2 + "px"};
+  overflow: hidden;
 `;
