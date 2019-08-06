6b9264f607fdf498d265fee8cf72fc33e24d9df0
Hossam <hossam.nasr99@gmail.com>
2019-08-07 01:02:20 +0200

    removed train container

--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Rodal from "rodal";
 import "rodal/lib/rodal.css";
-import { Container, Title, ButtonContainer, TrainContainer } from "./styles";
+import { Container, Title, ButtonContainer } from "./styles";
 import Train from "./components/Train";
 import AddCartButton from "./components/AddCartButton";
 import SizePicker from "./components/SizePicker";
@@ -47,9 +47,7 @@ const MainScreen = () => {
     <Container>
       <Title>ALL ABOARD THE HUMMUS TRAIN</Title>
       <SizePicker setCartSize={setCartSize} selectedSize={cartSize} />
-      <TrainContainer>
-        <Train carts={carts} cartSize={cartSize} />
-      </TrainContainer>
+      <Train carts={carts} cartSize={cartSize} />
       <ButtonContainer>
         <AddCartButton onClick={onButtonPress} />
       </ButtonContainer>
