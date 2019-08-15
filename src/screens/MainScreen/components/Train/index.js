21d712237e52a41daa144fac5c52298f2b83a869
Hossam <hossam.nasr99@gmail.com>
2019-08-15 04:19:54 +0200

    added transitions

--- src/screens/MainScreen/components/Train/index.js
+++ src/screens/MainScreen/components/Train/index.js
@@ -1,23 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, TrainImg } from "./styles";
+import { trainLeavingAudio } from "../../../../constants";
+import { CSSTransition } from "react-transition-group";
+import Button from "react-bootstrap/Button";
 import TrainHeadGif from "../../../../assets/img/train-head.gif";
 import TrainTailGif from "../../../../assets/img/train-tail.gif";
 import Cart from "./components/Cart";
 
-const Train = ({ carts, cartSize }) => (
-  <Container>
-    <TrainImg cartSize={cartSize} src={TrainHeadGif} />
-    {carts.map(({ name, color, timestamp, facepic }) => (
-      <Cart
-        key={timestamp}
-        name={name}
-        color={color}
-        facepic={facepic}
-        cartSize={cartSize}
-      />
-    ))}
-    <TrainImg cartSize={cartSize} src={TrainTailGif} />
-  </Container>
-);
+const audio = new Audio(trainLeavingAudio);
+
+const Train = ({ carts, cartSize }) => {
+  const [leaving, setLeaving] = useState(false);
+
+  useEffect(() => {
+    if (leaving) {
+      audio.play();
+    }
+  }, [leaving]);
+
+  return (
+    <Container>
+      <Button
+        onClick={() => {
+          setLeaving(!leaving);
+        }}
+      >
+        Hi
+      </Button>
+      <CSSTransition
+        key="train-head"
+        classNames="cart"
+        timeout={10000}
+        in={!leaving}
+        unmountOnExit
+      >
+        <TrainImg cartSize={cartSize} src={TrainHeadGif} />
+      </CSSTransition>
+      {carts.map(({ name, color, timestamp, facepic }) => (
+        <CSSTransition
+          key={timestamp}
+          classNames="cart"
+          timeout={10000}
+          in={!leaving}
+          unmountOnExit
+        >
+          <Cart
+            name={name}
+            color={color}
+            facepic={facepic}
+            cartSize={cartSize}
+          />
+        </CSSTransition>
+      ))}
+      <CSSTransition
+        key="train-tail"
+        classNames="cart"
+        timeout={10000}
+        in={!leaving}
+        unmountOnExit
+      >
+        <TrainImg cartSize={cartSize} src={TrainTailGif} />
+      </CSSTransition>
+    </Container>
+  );
+};
 
 export default Train;
